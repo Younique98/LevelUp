2160e53a80a6bddbdf382f6d84d093c4f00fec22
Erica Thompson <younique98@gmail.com>
2021-05-06 07:49:03 -0700

    fixed the network error 415 unsupported media type

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -3,6 +3,8 @@ from django.db import models
 class Game(models.Model):
 
     name = models.CharField(max_length=50)
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=50)
     number_of_players = models.IntegerField()
     skill_level = models.IntegerField()
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    
