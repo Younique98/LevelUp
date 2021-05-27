72a354236d642976a39d6b1438721a07f587971d
Erica Thompson <younique98@gmail.com>
2021-05-27 13:36:11 -0700

    created new report files for events

--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import usergame_list
+from .views import usergame_list, userevent_list
 from django.conf.urls import include
 
 urlpatterns = [
     path('reports/usergames', usergame_list),
+    path('reports/userevent', userevent_list),
     
 ]
--- levelupreports/views/__init__.py
+++ levelupreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .users.gamesbyuser import usergame_list
+from .users.eventsbyuser import userevent_list
 from .connection import Connection
